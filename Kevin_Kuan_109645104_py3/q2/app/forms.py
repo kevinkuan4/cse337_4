3194d576330f50223c4d1b39abb45f48d0227604
kevin <kevinkuan4@gmail.com>
2018-05-06 19:30:33 -0400

    need to fix error hadnling

--- Kevin_Kuan_109645104_py3/q2/app/forms.py
+++ Kevin_Kuan_109645104_py3/q2/app/forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, IntegerField, StringField, TextField
+from wtforms import SubmitField, IntegerField, StringField, TextField, DecimalField, RadioField
 from wtforms.validators import DataRequired
 
 
-class SubmissionForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    text = StringField('Text', validators=[DataRequired()])
 
-    operand1 = IntegerField('First number', validators=[DataRequired()])
-    operand2 = IntegerField('Second number', validators=[DataRequired()])
-    submit = SubmitField('Submit Entry')
+class SubmissionForm(FlaskForm):
+  
+    operand1 = StringField('First number', validators=[DataRequired()])
+    operand2 = StringField('Second number', validators=[DataRequired()])
+    operator = RadioField('Operator', choices=[('add','Add'),('subtract','Subtract'),('multiply','Multiply'),('divide','Divide')])
+    submit = SubmitField('Calculate')
--- Kevin_Kuan_109645104_py3/q2/app/routes.py
+++ Kevin_Kuan_109645104_py3/q2/app/routes.py
@@ -1,43 +1,68 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, url_for
 from forms import SubmissionForm
-
+result=0
 # from text_form import SubmissionForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'somesecretkey'
 
 
-
-
 @app.route('/')
+def default():
+	return redirect(url_for('index'))
 @app.route('/index',methods=['GET', 'POST'])
 def index():
+	global result
 	form = SubmissionForm()
 	if form.validate_on_submit():
-		return redirect(url_for('index'))
+		# print("hello")	
+		op1=float(form.operand1.data)
+		op2=float(form.operand2.data)
+		operator=(form.operator.data)
+
+		if operator=="add":
+			result= op1+op2
+			return redirect(url_for('add'))
+
+		elif operator=="subtract":
+			result= op1-op2
+			return redirect(url_for('subtract'))
+
+		elif operator=="multiply":
+			result= op1*op2
+			return redirect(url_for('multiply'))
+
+		else:
+			result= op1/op2		
+			return redirect(url_for('divide'))
+
 
 
 	return render_template("index.html",title="Form", form=form)
 
 @app.route("/result/add")
 def add():
-	return "Add!"
+	global result
+	return render_template('result.html',result=result)
 
 @app.route("/result/subtract")
 def subtract():
-	return "Subtract!"
+	global result
+	return render_template('result.html',result=result)
 
 @app.route("/result/multiply")
 def multiply():
-	return "Multiply!"
+	global result	
+	return render_template('result.html',result=result)
 
 @app.route("/result/divide")
 def divide():
-	return "Divide!"
+	global result	
+	return render_template('result.html',result=result)
 
 @app.route("/result/error")
 def input_error():
-	return "Invalid numbers!"
+    return render_template('error.html')
 
 @app.errorhandler(404)
 def not_found_error(error):
